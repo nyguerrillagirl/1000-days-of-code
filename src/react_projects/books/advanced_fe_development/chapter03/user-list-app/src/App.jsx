fa2b03d6a309af2d01fc12f8646fa0732bdc2031
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2025-07-29 06:25:51 -0400

    Day #305 - Resumed Freecodecamp course + Datacamp + FE book (all CSS file missing!!!)

--- src/react_projects/books/advanced_fe_development/chapter03/user-list-app/src/App.jsx
+++ src/react_projects/books/advanced_fe_development/chapter03/user-list-app/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import React from 'react';
+import UserList from './components/UserList';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([
+    { id: 1, name: 'John Doe', email: 'john_doe@brainycode.com', isActive: true },
+    { id: 2, name: 'Jane Smith', email: 'jane_smith@brainycode.com', isActive: false },
+    { id: 3, name: 'Alice Johnson', email: 'alice_johnson@brainycode.com', isActive: true }
+  ]);
+
+  const toggleUserActivity = (id) => {
+    setUsers( (prevUsers) =>
+      prevUsers.map(user =>
+        user.id === id ? { ...user, isActive: !user.isActive } : user
+      )
+    );
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <UserList users={users} toggleActivity={toggleUserActivity} />
+    </div>
   )
 }
 
