f181645231692bd7742707fb86b9eee67589c53f
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2025-11-03 07:13:05 -0500

    Day # 402 - Started Sonic Runner Game!

---
+++ src/html_css_javascipt_projects/sonic-runner-game/README.md
@@ -0,0 +1,6 @@
+1. Open the project in Visual Studio Code
+2. Open a Terminal Window in VSC
+3. Navigate to the project directory
+4. Enter: npm run dev
+5. Open a browser
+6. Enter http://localhost:5173
---
+++ src/html_css_javascipt_projects/sonic-runner-game/src/kaplayCtx.js
@@ -0,0 +1,20 @@
+import kaplay from 'kaplay';
+
+export const k = kaplay({
+  width: 1920,
+  height: 1080,
+  letterbox: true,
+  background: [0, 0, 0],
+  global: false,
+  touchToMouse: true,
+  buttons: {
+    jump: {
+        keyboard: ['Space'],
+        mouse: ['Left'],
+    }
+  },
+  debugKey: 'd',
+  debug: true,
+});
+
+export default k;
