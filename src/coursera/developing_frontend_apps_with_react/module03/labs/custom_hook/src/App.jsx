ffb6baaf151d0918a3970d1301c1a803b5e1d2e3
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2025-12-01 06:59:22 -0500

    Day #430 - Datacamp (finally addresses assessment)+Coursera React + RealPython. Grade: C

---
+++ src/coursera/developing_frontend_apps_with_react/module03/labs/custom_hook/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import FetchData from './Components/FetchData'
+import FetchYogaData from './Components/FetchYogaData'
+
+function App() {
+  return (
+ <>
+ <FetchYogaData/>
+ </>
+  )
+}
+
+export default App
