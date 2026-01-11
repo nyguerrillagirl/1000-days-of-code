95bec4a48136dcb98741326b4f1d9ed63dc05d9c
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2026-01-11 07:24:39 -0500

    Day #470 - Datacamp+React+Arduino

---
+++ src/react_projects/books/react_comprehensive_guide/chapter04/Listing06/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import MyComponent from './MyComponent';
+
+function App() {
+  return (
+    <div>
+      <h1>Books management</h1>
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/react_projects/books/react_comprehensive_guide/chapter04/Listing08/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Timer from './Timer';
+
+function App() {
+  return (
+    <div>
+      <h1>Books management</h1>
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
