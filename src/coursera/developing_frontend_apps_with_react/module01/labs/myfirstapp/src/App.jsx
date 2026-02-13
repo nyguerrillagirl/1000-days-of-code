599c2f96918763753a2bf36e85123222b96a7b5b
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2026-02-13 06:50:53 -0500

    Day #503 - Articles+Datacamp!+Coursera courses review+React+Reading

---
+++ src/coursera/developing_frontend_apps_with_react/module01/labs/myfirstapp/src/App.jsx
@@ -0,0 +1,11 @@
+function App( props ) {
+  const currDate = new Date();
+  return (
+    <div> 
+      <h1>Hello, World!</h1>
+      <h2>The time is now is {currDate.toLocaleTimeString()}</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/coursera/developing_frontend_apps_with_react/module01/labs/timer/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Clock from './Clock'
+
+function App() {
+  return (
+    <>
+      <Clock />
+    </>
+  )
+}
+
+export default App
