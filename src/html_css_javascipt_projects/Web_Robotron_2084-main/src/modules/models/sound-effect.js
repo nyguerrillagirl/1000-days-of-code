36cbb8a05b8c36b04a8bcf18a4495ffbc54acea9
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2026-05-28 03:09:25 -0400

    Remove src/html_css_javascipt_projects after extracting to its own repo

--- src/html_css_javascipt_projects/Web_Robotron_2084-main/src/modules/models/sound-effect.js
+++
@@ -1,9 +0,0 @@
-export { SoundEffect };
-
-class SoundEffect {
-    constructor() {
-        this.sound = null;
-        this.priority = 0;
-        this.timeout = null;
-    }
-}
--- src/html_css_javascipt_projects/Web_Robotron_2084-main/src/modules/models/sprite.js
+++
@@ -1,8 +0,0 @@
-export { Sprite };
-
-class Sprite {
-    constructor(spriteSrc, spritesIndex) {
-        this.spritesheet = new Image();
-        this.spritesheet.src = spritesIndex[spriteSrc];
-    }
-}
